9287eb872cd7072e86435f23e739e3aa2fe3a280
João Guilherme <126221124+sopena@users.noreply.github.com>
2024-11-17 19:52:36 -0300

    Add files via upload

--- binary_tree.py
+++ binary_tree.py
@@ -46,19 +46,7 @@ class Arvore:
             self.esq.imprimirPosEsq()
         print(self.dado)
 
-    def localizar(self, v):
-        if v == self.dado:
-            return self
-        if self.esq:
-            aux = self.esq.localizar(v)
-            if aux:
-                return aux
-        if self.dir:
-            aux = self.dir.localizar(v)
-            if aux:
-                return aux
-        else:
-            return None
+    
 
     def imprimir(self, lado, metodo):
         if lado == "esq" and metodo == "pre":
@@ -87,7 +75,7 @@ class Arvore:
                 print("Adicionado com sucesso!")
             else: print("Já existe um nodo nesse lugar")
     
-    def localizar_pai(self, v):
+    def localizar(self, v):
         if v == self.dado:
             return self
         if self.esq:
@@ -101,6 +89,47 @@ class Arvore:
         else:
             return None
         
+    def localizar_pai(self, v):
+        if v == self.dado:
+            return self
+        if self.esq:
+            if self.esq.dado == v:
+                return self
+            aux = self.esq.localizar_pai(v)
+            if aux:
+                return aux
+        if self.dir:
+            if self.dir.dado == v:
+                print("funcionou")
+                return self
+            aux = self.dir.localizar_pai(v)
+            if aux:
+                return aux
+        else:
+            return None
+        
+    def excluir(self, v):
+        pai = self.localizar_pai(v)
+        if pai.esq != None:
+            if pai.esq.dado == v:
+                if pai.esq.esq != None:
+                    pai.esq = pai.esq.esq
+                if pai.esq.dir != None:
+                    pai.esq = pai.esq.dir
+                if pai.esq.esq == None and pai.esq.dir == None:
+                    pai.esq = None
+                print("excluido")
+        if pai.dir != None:
+            if pai.dir.dado == v:
+                if pai.dir.dir != None:
+                    pai.dir = pai.dir.dir
+                if pai.dir.esq != None:
+                    pai.dir = pai.dir.esq
+                if pai.dir.dir == None and pai.dir.esq == None:
+                    pai.dir = None
+                print("excluido")
+        
+        
 
 a = Arvore("A")
 a.esq = Arvore("B")
@@ -110,12 +139,19 @@ a.esq.esq.esq = Arvore("F")
 a.esq.esq.dir = Arvore("E")
 a.dir = Arvore("C")
 #a.imprimir("esq", "pre")
-#pai = a.localizar("G")
-#if pai == None:
+#nodo = a.localizar("G")
+#if nodo == None:
 #    print("Esse valor não está na árvore")
 #else: 
-#    print(pai.dado)
+#    print(nodo.dado)
 
 a.inserir("C", "dir", "H")
 a.inserir("H", "esq", "W")
+#a.imprimir("esq", "pre")
+
+pai_de_g = a.localizar_pai("W")
+
+#a.excluir("H")
+#a.excluir("C")
+a.excluir("W")
 a.imprimir("esq", "pre")
